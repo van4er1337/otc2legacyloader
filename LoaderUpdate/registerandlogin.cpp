0994a8f92b7eb4e696fb4ce61cd3519fa0906675
van4er1337 <77556732+van4er1337@users.noreply.github.com>
2023-11-04 21:45:00 +0300

    Update registerandlogin.cpp

--- LoaderUpdate/registerandlogin.cpp
+++ LoaderUpdate/registerandlogin.cpp
@@ -1,37 +1,7 @@
 #include "registerandlogin.h"
 #include <fstream>
 #include <iostream>
-#include "E:\1\httplib.h" // Путь к библиотеке httplib
-
-bool blockUserIP(const std::string& ipAddress, const std::string& reason) {
-    httplib::Client cli("http://van4er8922.temp.swtest.ru");  // Замените на адрес вашего сервера
-
-    std::string path = "/site/Ban.php"; // Замените на путь на сервере для блокировки IP-адреса
-    httplib::Params params = {
-        { "ip", ipAddress },
-        { "reason", reason }
-    };
-
-    auto res = cli.Post(path.c_str(), params);
-
-    if (res && res->status == 200) {
-        std::string response_body = res->body;
-        std::cout << "Response body: " << response_body << std::endl;
-
-        if (response_body == "IP blocked successfully!") {
-            std::cout << "IP blocked successfully!" << std::endl;
-            return true; // IP-адрес заблокирован
-        }
-        else {
-            std::cout << "Failed to block IP." << std::endl;
-            return false; // Не удалось заблокировать IP-адрес
-        }
-    }
-    else {
-        std::cout << "Failed to connect to the server." << std::endl;
-        return false; // Ошибка соединения с сервером
-    }
-}
+#include "E:\1\httplib.h" // ГЏГіГІГј ГЄ ГЎГЁГЎГ«ГЁГ®ГІГҐГЄГҐ httplib
 
 
 
@@ -41,7 +11,7 @@ bool registerUser(const std::string& username, const std::string& password) {
         return false;
     }
 
-    httplib::Client cli("http://van4er8922.temp.swtest.ru");
+    httplib::Client cli("your website");
 
     std::string query = "/Register.php?username=" + username + "&password=" + password;
 
@@ -53,22 +23,22 @@ bool registerUser(const std::string& username, const std::string& password) {
 
         if (response_body == "User added successfully!") {
             std::cout << "User added successfully!" << std::endl;
-            return true; // Пользователь добавлен
+            return true; // ГЏГ®Г«ГјГ§Г®ГўГ ГІГҐГ«Гј Г¤Г®ГЎГ ГўГ«ГҐГ­
         }
         else {
             std::cout << "Failed to add user." << std::endl;
-            return false; // Не удалось добавить пользователя
+            return false; // ГЌГҐ ГіГ¤Г Г«Г®Г±Гј Г¤Г®ГЎГ ГўГЁГІГј ГЇГ®Г«ГјГ§Г®ГўГ ГІГҐГ«Гї
         }
     }
     else {
         std::cout << "Failed to connect to the server." << std::endl;
-        return false; // Ошибка соединения
+        return false; // ГЋГёГЁГЎГЄГ  Г±Г®ГҐГ¤ГЁГ­ГҐГ­ГЁГї
     }
 }
 
 
 bool authenticateUser(const std::string& username, const std::string& password) {
-    httplib::Client cli("http://van4er8922.temp.swtest.ru");
+    httplib::Client cli("your website");
 
     std::string query = "/Login.php?username=" + username + "&password=" + password;
 
@@ -80,15 +50,15 @@ bool authenticateUser(const std::string& username, const std::string& password) 
 
         if (response_body == "Yes!") {
             std::cout << "Yes!" << std::endl;
-            return true; // Пользователь аутентифицирован
+            return true; // ГЏГ®Г«ГјГ§Г®ГўГ ГІГҐГ«Гј Г ГіГІГҐГ­ГІГЁГґГЁГ¶ГЁГ°Г®ГўГ Г­
         }
         else {
             std::cout << "Invalid username or password." << std::endl;
-            return false; // Неудачная аутентификация
+            return false; // ГЌГҐГіГ¤Г Г·Г­Г Гї Г ГіГІГҐГ­ГІГЁГґГЁГЄГ Г¶ГЁГї
         }
     }
     else {
         std::cout << "Failed to connect to the server." << std::endl;
-        return false; // Ошибка соединения
+        return false; // ГЋГёГЁГЎГЄГ  Г±Г®ГҐГ¤ГЁГ­ГҐГ­ГЁГї
     }
 }
